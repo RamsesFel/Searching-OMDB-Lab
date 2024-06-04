7c3d5397bb7b054330b0a9e25e57c1d9b12c307e
Ramses Feliciano <ramsesfeliciano@gmail.com>
2024-06-04 16:54:05 -0400

    first commit

---
+++ Searching OMDB Lab/Models/MovieDAL.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace Searching_OMDB_Lab.Models
+{
+    public class MovieDAL
+    {
+        public static MovieModel GetMovie(string Movie) 
+        {
+            string url = $"http://www.omdbapi.com/?apikey={Secret.apiKey}&s={Movie}"; 
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+
+            MovieModel result = JsonConvert.DeserializeObject<MovieModel>(JSON);
+            return result;
+        }
+    }
+}
